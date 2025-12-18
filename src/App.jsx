0866d79d00b82d97eee23361ba648d455af4d71b
Bassam Mahmoud <bassam223mahmoud@gmail.com>
2025-12-18 16:10:59 +0200

    Update Available file and Project Structuers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./Components/UI/Header/Header";
+import AvailableMeals from "./Components/UI/main/AvailableMeals";
+
 function App() {
   return (
     <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <Header />
+      <AvailableMeals />
     </>
   );
 }
