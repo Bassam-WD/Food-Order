7e0db93cc2248e8349355c125766e140f082eb29
Bassam Mahmoud <bassam223mahmoud@gmail.com>
2025-12-24 19:54:46 +0200

    update useFetch hook

--- frontend/src/Hooks/useFetch.js
+++ frontend/src/Hooks/useFetch.js
@@ -1,29 +1,56 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-export default function useFetch(url) {
-  const [data, setData] = useState([]);
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(response.message || "Something went wrong, faild to send request");
+  }
+
+  return resData
+
+}
+
+
+
+export default function useFetch(url, config, inValue) {
+  const [data, setData] = useState(inValue);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsLoading(true)
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error("Faild To Featch Meals");
-        }
-        const data = await response.json();
-        setData(data)
-      } catch (error) {
-        setError(error.message)
-      } finally {
-        setIsLoading(false)
-      }
+  const [error, setError] = useState();
 
-      console.log(data, isLoading, error)
+  // clear data function to use it when Order succes
+  function clearData() {
+    setData(inValue)
+  }
+
+
+  const sendRequest = useCallback(async function sendRequest(data) {
+    setIsLoading(true);
+    try {
+      const resData = await sendHttpRequest(url, { ...config, body: data });
+      setData(resData);
+      console.log(resData, 'resdata');
+
+    } catch (error) {
+      setError(error.message || "Something went wrong");// to sure just be safe.
+    } finally {
+      setIsLoading(false);
     }
-    fetchData();
-  }, [url]);
-  return { data, isLoading, error };
+  }
+    , [url, config])
+
+
+
+  useEffect(() => {
+    if (config && (config.method === "GET" || !config.method) || !config) {
+      sendRequest();
+    }
+
+  }, [sendRequest, config]);
+
+
+
+  return { data, isLoading, error, sendRequest , clearData};
 
 }
